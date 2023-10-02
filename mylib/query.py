dba42aab643921fb8fec7f94e1901a252050833c
jiwonny29 <141780408+jiwonny29@users.noreply.github.com>
2023-10-02 01:50:36 +0000

    update files

--- mylib/query.py
+++ mylib/query.py
@@ -3,11 +3,13 @@
 import sqlite3
 
 
-def read_data():
-    """read data from the databaseery the database for the top 5 rows of the CarsDB table"""
+def query():
+    """Read data from the database for the top 10 rows of the 'cost' table."""
     conn = sqlite3.connect("cost.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM cost LIMIT 10")
+    cursor.execute(
+        "SELECT * FROM cost LIMIT 10"
+    )  # Use 'cost' as the table name
     print("Top 10 rows of the 'cost' table:")
     print(cursor.fetchall())
     conn.close()
