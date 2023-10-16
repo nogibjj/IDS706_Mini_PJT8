775253c42e57157099a93753f0a05098115ef22c
jiwonny29 <141780408+jiwonny29@users.noreply.github.com>
2023-10-16 18:37:48 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -35,7 +35,7 @@ class TestMain(unittest.TestCase):
             "a.share_population_in_metro_areas, "
             "b.gini_index "
             "FROM default.hate_crimes1DB AS a "
-            "JOIN default.hate_crimes2DB AS b ON a.state = b.state "
+            "JOIN default.hate_crimes2DB AS b ON a.id = b.id "
             "GROUP BY a.state, a.share_population_in_metro_areas, b.gini_index "
             "ORDER BY b.gini_index "
             "LIMIT 5"
