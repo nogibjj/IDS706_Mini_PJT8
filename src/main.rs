1b3424f68da037c1293d7c33616014040e724a2e
jiwonny29 <141780408+jiwonny29@users.noreply.github.com>
2023-10-24 19:45:47 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-mod lib; // Import the lib module
 use csv::ReaderBuilder;
 use std::error::Error;
 use std::fs::File;
@@ -7,23 +6,19 @@ use std::time::Instant;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let start_time = Instant::now();
-    // 1. Read the cars.csv file
-    let file = File::open("cars.csv")?;
+    let file = File::open("heightweight.csv")?;
 
-    // Create the CSV reader with the specified delimiter
     let mut rdr = ReaderBuilder::new()
-        .delimiter(b';') // Set the delimiter to ;
+        .delimiter(b';') 
         .has_headers(true)
         .from_reader(file);
 
-    // Find the index of the "Weight" column
     let headers = rdr.headers()?;
     let weight_index = headers
         .iter()
         .position(|h| h == "Weight")
         .ok_or("Weight column not found")?;
 
-    // 2. Extract the "Weight" column from the CSV data
     let mut weights: Vec<f64> = Vec::new();
     for result in rdr.records() {
         let record = result?;
@@ -34,7 +29,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    // 3. Compute the statistics
     let stats = lib::compute_statistics(&weights);
     println!("Mean: {}", stats.mean);
     println!("Median: {}", stats.median);
@@ -42,16 +36,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Size: {}", stats.size);
     let end_time = Instant::now();
 
-    // Calculate the elapsed time and resource usage
     let elapsed_time = end_time.duration_since(start_time);
-    println!("Total execution time: {:?}", elapsed_time); // Print the elapsed time
-                                                          // Memory usage
+    println!("Total execution time: {:?}", elapsed_time); 
+                                                         
     let mem_info = sys_info::mem_info().unwrap();
     println!(
         "Memory Usage: {}%",
         (mem_info.total - mem_info.avail) as f32 / mem_info.total as f32 * 100.0
     );
-    // CPU calculation
+
     let output = Command::new("ps")
         .arg("-o")
         .arg("%cpu")
@@ -60,11 +53,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         .output()
         .expect("Failed to execute ps command");
 
-    // Convert the output to a string
     let usage = String::from_utf8_lossy(&output.stdout);
     let lines: Vec<&str> = usage.split('\n').collect();
 
-    // Parse the CPU usage from the output
     if lines.len() >= 2 {
         let usage_str = lines[1].trim();
         let usage_float: Result<f32, _> = usage_str.parse();
